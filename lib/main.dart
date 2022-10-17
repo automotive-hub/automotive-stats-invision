107b60b2e4a41b3e7f9c0385cac6f0310a1cedb3
Duy <coduy96@gmail.com>
2022-10-17 23:18:18 +0700

    remove unuse components

--- lib/main.dart
+++ lib/main.dart
@@ -8,17 +8,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
